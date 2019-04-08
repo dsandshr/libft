cbc468f3dcdc6550ace10e629689182721ec346a
Draugr Sandshrew <dsandshr@mi-t2.21-school.ru>
2019-04-08 17:52:46 +0300

    URA

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,31 +6,37 @@
 /*   By: dsandshr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 14:49:56 by dsandshr          #+#    #+#             */
-/*   Updated: 2019/04/08 15:02:12 by dsandshr         ###   ########.fr       */
+/*   Updated: 2019/04/08 17:48:16 by dsandshr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
+#include <string.h>
 
 char	*ft_strtrim(char const *s)
 {
+	int j;
 	int i;
 	char *s2;
-	int j;
 
 	i = 0;
-	while (s[i] == ' ' && s[i] == '\n' && s[i] == '\t')
+	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
 		i++;
+	if (s[i] == '\0')
+		return (s2 = ft_strnew(0));
 	j = i;
 	while (s[j])
 		j++;
-	while (s[j] == ' ' && s[j] == '\n' && s[i] == '\t')
+	while (s[j - 1] == ' ' || s[j - 1] == '\t' || s[j - 1] == '\n')
 		j--;
 	s2 = ft_strnew(j - i);
 	if (s2 == NULL)
 		return (NULL);
-	while (i <	j)
+	s = s + i;
+	j = j - i;
+	i = 0;
+	while (i < j)
 	{
 		s2[i] = s[i];
 		i++;
