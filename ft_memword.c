cb1f7732f2d825482298e21f986aa44e0b4d0449
Draugr Sandshrew <dsandshr@mi-t4.21-school.ru>
2019-04-11 18:24:48 +0300

    +ft_strsplit.c URA

---
+++ ft_memword.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memword.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsandshr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/11 17:36:08 by dsandshr          #+#    #+#             */
+/*   Updated: 2019/04/11 17:39:28 by dsandshr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_memword(const char *str, char c, int *i)
+{
+	char *s;
+	int k;
+
+	s = (char *)malloc(sizeof(s) * (ft_strlen(str)));
+	if (s == NULL)
+		return (NULL);
+	k = 0;
+	while (str[*i] != c && str[*i])
+	{
+		s[k] = str[*i];
+		k++;
+		*i += 1;
+
+	}
+	s[k] = '\0';
+	while (str[*i] == c && str[*i])
+		*i += 1;
+	return (s);
+}
---
+++ ft_strsplit.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsandshr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/11 16:58:51 by dsandshr          #+#    #+#             */
+/*   Updated: 2019/04/11 17:38:36 by dsandshr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char			**ft_strsplit(const char *str, char c)
+{
+	int		i;
+	int		j;
+	int		w;
+	char	**s;
+
+	i = 0;
+	j = 0;
+	w = ft_countword(str, c);
+	s = (char **)malloc(sizeof(s) * (ft_countword(str, c) + 2));
+	if (s == NULL)
+		return (NULL);
+	while (str[i] == c && str[i])
+		i++;
+	while (j < w && str[i])
+	{
+		s[j] = ft_memword(str, c, &i);
+		j++;
+	}
+	s[j] = NULL;
+	return (s);
+}
