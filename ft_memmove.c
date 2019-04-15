72b3e8ca525216153498076815ef4349af5e50b0
Draugr Sandshrew <dsandshr@mi-u3.21-school.ru>
2019-04-15 16:42:01 +0300

    FINISH

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: dsandshr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/06 21:15:49 by dsandshr          #+#    #+#             */
-/*   Updated: 2019/04/14 20:35:25 by dsandshr         ###   ########.fr       */
+/*   Updated: 2019/04/15 16:09:54 by dsandshr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,19 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	size_t i;
 
 	i = 0;
-	if (((unsigned char *)src < ((unsigned char *)dst)))
-		while ((int)--len >= 0)
-		{
-			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
-		}
-	else
-		while (i < len)
-		{
-			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-			i++;
-		}
+	if (dst || src)
+	{
+		if (((unsigned char *)src < ((unsigned char *)dst)))
+			while ((int)--len >= 0)
+			{
+				((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
+			}
+		else
+			while (i < len)
+			{
+				((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+				i++;
+			}
+	}
 	return (dst);
 }
